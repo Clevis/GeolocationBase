22d91ae94439192b6c3b036ddb22fb2091975342
Vlasta Neubauer <paranoiq@centrum.cz>
2013-09-11 10:08:48 +0200

    fixed tests boot

--- tests/boot.php
+++ tests/boot.php
@@ -4,12 +4,8 @@ require __DIR__ . '/../vendor/nette/tester/Tester/bootstrap.php';
 require __DIR__ . '/../vendor/autoload.php';
 
 require __DIR__ . '/../src/Geolocation/interfaces/IArea.php';
-require __DIR__ . '/../src/Geolocation/interfaces/IAddressService.php';
-require __DIR__ . '/../src/Geolocation/interfaces/IAddressCache.php';
-require __DIR__ . '/../src/Geolocation/interfaces/IElevationService.php';
-require __DIR__ . '/../src/Geolocation/interfaces/IElevationCache.php';
-require __DIR__ . '/../src/Geolocation/interfaces/IPositionService.php';
-require __DIR__ . '/../src/Geolocation/interfaces/IPositionCache.php';
+require __DIR__ . '/../src/Geolocation/interfaces/IGeocodingService.php';
+require __DIR__ . '/../src/Geolocation/interfaces/IGeocodingCache.php';
 
 require __DIR__ . '/../src/Geolocation/exceptions.php';
 
@@ -19,7 +15,6 @@ require __DIR__ . '/../src/Geolocation/Rectangle.php';
 require __DIR__ . '/../src/Geolocation/Address.php';
 require __DIR__ . '/../src/Geolocation/Geocoder.php';
 
-require __DIR__ . '/../src/Geolocation/Google/ElevationClient.php';
 require __DIR__ . '/../src/Geolocation/Google/GeocodingClient.php';
 require __DIR__ . '/../src/Geolocation/Google/GeocodingResponse.php';
 

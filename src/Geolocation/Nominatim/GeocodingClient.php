c73f4fc5a434c132c70a163482c9386acd0b67b0
Tomas Susanka <tsusanka@gmail.com>
2014-04-01 10:17:59 +0200

    Nominatim: cosmetics to match clevis' code standards

--- src/Geolocation/Nominatim/GeocodingClient.php
+++ src/Geolocation/Nominatim/GeocodingClient.php
@@ -16,34 +16,42 @@ use Clevis\Geolocation\InvalidResponseException;
 /**
  * Client for Nominatim Geocoding API
  *
- * Nominatim.openstreetmap.org "requirements
- * No heavy uses (an absolute maximum of 1 request/s).
- * Provide a valid HTTP Referer or User-Agent identifying the application (stock User-Agents as set by http libraries will not do).
- * Clearly display attribution as suitable for your medium.
- * Data is provided under the ODbL licence which requires to share alike (although small extractions are likely to be covered by fair usage / fair dealing)."
+ * Nominatim.openstreetmap.org requirements:
+ * - No heavy uses (an absolute maximum of 1 request/s).
+ * - Provide a valid HTTP Referer or User-Agent identifying the application (stock User-Agents as set by http libraries will not do).
+ * - Clearly display attribution as suitable for your medium.
+ * - Data is provided under the ODbL licence which requires to share alike (although small extractions are likely to be covered by fair usage / fair dealing)."
  *
  * Own instances of nominatim are not subject to usage restrictions (except for the license, 'course).
  *
  * May be overridden for different nominatim instances (see $base_url)
- * 
+ *
  * @author Jan Martinec
  */
 class GeocodingClient extends Object implements IGeocodingService
 {
 
-	protected $base_url = 'http://nominatim.limes.dyndns.org:19401/'; // use our own nominatim instance, instead of the public one
+	/** @var string  we use our own nominatim instance, instead of the public one */
+	protected $baseUrl = 'http://nominatim.limes.dyndns.org:19401/';
 
-	protected $user_name = 'rekolacz'; // set to empty for "no HTTP auth required"
-	protected $user_pwd = 'rekolacz';
+	/** @var string  username for HTTP auth; set to empty for "no HTTP auth required"; just a simple restriction not a real security */
+	protected $username = 'rekolacz';
 
-	protected $email = 'info@rekola.cz'; // needed in case of capacity problems (not in case of own instance)
-	protected $ua = 'ReKolaSMS http://rekola.cz/ info@rekola.cz 1.0 2014-01-07'; // User-Agent string; required!
+	/** @var string  password for HTTP auth */
+	protected $password = 'rekolacz';
+
+	/** @var string  email needed in case of capacity problems (not in case of own instance) */
+	protected $email = 'info@rekola.cz';
+
+	/** @var string  User-Agent string; required! */
+	protected $ua = 'ReKolaSMS http://rekola.cz/ info@rekola.cz 1.0 2014-01-07';
 
 	/**
 	 * allows use of other nominatims
 	 */
-	public function setBaseUrl($url) {
-		$this->base_url = $url;
+	public function setBaseUrl($url)
+	{
+		$this->baseUrl = $url;
 	}
 
 	/**
@@ -139,13 +147,13 @@ class GeocodingClient extends Object implements IGeocodingService
 		if ($query instanceof Position)
 		{
 			$options['lat'] = $query->latitude;
-			$options['lon'] = $query->longitude; // NB: not "lng"!
+			$options['lon'] = $query->longitude;
 		}
 		else
 		{
-			$options['lat'] = null;
-			$options['lon'] = null;
-			$options['q'] = ((string) $query); // NB: not "address"!
+			$options['lat'] = NULL;
+			$options['lon'] = NULL;
+			$options['q'] = ((string) $query);
 		}
 
 		return $this->query($options);
@@ -161,9 +169,12 @@ class GeocodingClient extends Object implements IGeocodingService
 	 */
 	protected function query(array $options)
 	{
-		if ($options['lat'] && $options['lon']) {
+		if ($options['lat'] && $options['lon'])
+		{
 			$method = 'reverse';
-		} else {
+		}
+		else
+		{
 			$method = 'search';
 		}
 
@@ -171,16 +182,18 @@ class GeocodingClient extends Object implements IGeocodingService
 		$options['addressdetails'] = 1;
 		$options['email'] = $this->email;
 
-		$url = $this->base_url . $method . '?' . http_build_query($options);
+		$url = $this->baseUrl . $method . '?' . http_build_query($options);
 
 		$curl = curl_init();
 
 		curl_setopt($curl, CURLOPT_HTTPHEADER, Array('Content-Type: application/json; charset=utf-8'));
 
-		if ($this->user_name) {
-			curl_setopt($curl, CURLOPT_USERPWD, $this->user_name . ':' . $this->user_pwd);
+		if ($this->username)
+		{
+			curl_setopt($curl, CURLOPT_USERPWD, $this->username . ':' . $this->password);
 		}
-		if ($this->ua) {
+		if ($this->ua)
+		{
 			curl_setopt($curl, CURLOPT_USERAGENT,$this->ua);
 		}
 		curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
@@ -195,7 +208,8 @@ class GeocodingClient extends Object implements IGeocodingService
 		}
 		$payload = @json_decode($response); // @ - intentionally to escalate error to exception
 
-		if ($payload && !is_array($payload)) {
+		if ($payload && !is_array($payload))
+		{
 			$payload = array($payload);
 		}
 
@@ -205,7 +219,8 @@ class GeocodingClient extends Object implements IGeocodingService
 			{
 				throw new InvalidStatusException("Geocoding query failed (no results).");
 
-			} else
+			}
+			else
 			{
 				throw new InvalidResponseException("Unable to parse response from geocoding API.");
 			}
